4c698b0f5fff497c7a9b7c34ef98a26f64800832
sride <sride@LAPTOP-23SR8A15>
2026-01-02 16:43:26 +0530

    First Commit for jenkisPractice project

---
+++ src/test/java/githhubpractice/GithubPracticeTest.java
@@ -0,0 +1,13 @@
+package githhubpractice;
+
+import org.testng.annotations.Test;
+
+public class GithubPracticeTest {
+@Test
+	public void  githubPracticeTest() {
+		System.out.println("Hi github");
+	}
+
+	}
+
+
